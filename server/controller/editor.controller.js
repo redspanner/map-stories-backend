86e182abdbc4f6dc026d5c0ac4c90f5d1e755d5a
Isabella Chen <miss.izzyc@gmail.com>
2017-10-13 16:54:26 +0200

    FEAT: create editor and find editor functions created on editor controller

--- server/controller/editor.controller.js
+++ server/controller/editor.controller.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose');
 const Editor = require('../model/editor.model');
 
 const createEditor = async (ctx, next) => {
-  try {
-    const editorData = {
-      name: ctx.request.body.name,
-      avatar: ctx.request.body.avatar
-    };
-    const editor = await Editor.createEditor(editorData);
-    ctx.status = 200;
-  } catch (e) {
-    console.log(error);
-  }
+  const editorData = {
+    name: ctx.request.body.name,
+    avatar: ctx.request.body.avatar
+  };
+  const editor = await Editor.createEditor(editorData);
+  ctx.status = 200;
+};
+
+const findEditor = async (ctx, next) => {
+  const editorData = {
+    name: ctx.request.body.name,
+    avatar: ctx.request.body.avatar
+  };
+  const editor = await Editor.createEditor(editorData);
+  ctx.status = 200;
 };
 
 module.exports = {
